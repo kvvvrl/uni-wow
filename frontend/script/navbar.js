653fb784696f7c4e0fa516aa07440220ae3f24f8
Milan <milanesser9000@gmail.com>
2025-04-18 14:17:33 +0200

    changed navbar color

--- frontend/script/navbar.js
+++ frontend/script/navbar.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", function () {
     let opacity = 1 - Math.min(scrollPosition / maxScroll, 1); // Reduziert die Transparenz schrittweise
 
     // Setze die Hintergrundfarbe der Navbar mit der berechneten Transparenz
-    navbar.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+    navbar.style.backgroundColor = `rgba(54, 54, 54, ${opacity})`;
     //Wenn gewisse Transparenz das ist soll die bleiben
     if(opacity < 0.6){
             navbar.style.backgroundColor = `rgba(0, 0, 0, 0.6)`;
